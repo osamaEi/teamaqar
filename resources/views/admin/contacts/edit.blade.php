f83f6500e8d221625bdb85a93528d6d80361330d
osama bakry <o.bakry@loop.sa>
2026-01-13 16:14:24 +0200

    done

--- resources/views/admin/contacts/edit.blade.php
+++ resources/views/admin/contacts/edit.blade.php
@@ -11,7 +11,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">الرئيسية</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard.page') }}">الرئيسية</a></li>
                         <li class="breadcrumb-item"><a href="{{ route('contacts.index') }}">العملاء</a></li>
                         <li class="breadcrumb-item active">تعديل</li>
                     </ol>
--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -11,7 +11,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">الرئيسية</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard.page') }}">الرئيسية</a></li>
                         <li class="breadcrumb-item active">العملاء</li>
                     </ol>
                 </div>
